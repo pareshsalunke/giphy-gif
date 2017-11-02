9de9dfd2a9ec6919b79f8bae3052f23f6525e091
Paresh Salunke <pareshsalunke@gmail.com>
2017-11-01 20:58:56 -0400

    Battery Dying

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import request from 'superagent';
 //Custom File imports
 import SearchBar from './components/SearchBar';
 import GifList from './components/GifList';
+import GifModal from './components/GifModal';
 import './styles/app.css';
 
 const API_KEY = "dJkVfg0pxoWLpo6Dh6HUY5fcifL6JJoU";
@@ -19,10 +20,27 @@ class App extends Component {
 		super();
 
 		this.state = {
-			gifs: []
+			gifs: [],
+			selectedGif: null,
+			modalIsOpen: false
+
 		};
 
 		this.handleTermChange = this.handleTermChange.bind(this);
+	}
+
+	openModal(gif) {
+		this.setState({
+			modalIsOpen: true,
+			selectedGif: gif
+		});
+	}
+
+	closeModal() {
+		this.setState({
+			modalIsOpen: false,
+			selectedGif: null
+		})
 	}
 
 	handleTermChange(term) {
@@ -39,6 +57,9 @@ class App extends Component {
 			<div>
 				<SearchBar onTermChange={term => this.handleTermChange(term)} />
 				<GifList gifs = {this.state.gifs} />
+				<GifModal   modalIsOpen={this.state.modalIsOpen}
+							selectedGif={this.state.selectedGif}
+							onRequestClose = {() => this.closeModal()} />
 			</div>
 		);
 	}
