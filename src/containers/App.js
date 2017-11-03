7a0889230b942364a5a9cf54ac07515cbf309c20
Paresh Salunke <pareshsalunke@gmail.com>
2017-11-03 10:24:33 -0400

    Bind store to container

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,3 +1,24 @@
 /**
  * Created by ParryPC on 11/2/17.
  */
+import React, { Component } from 'react';
+import GifsTemp from '../components/GifsTemp';
+import { connect } from 'react-redux';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<GifsTemp gifs = { this.props.gifs } />
+			</div>
+		);
+	}
+}
+
+function mapStateToProps({gifs}) {
+	return {
+		gifs
+	};
+}
+
+export default connect(mapStateToProps)(App);
