f503a28c234391e79db1db070ad47fb14036d625
Paresh Salunke <pareshsalunke@gmail.com>
2017-11-03 22:06:28 -0400

    Updated the README file to provide instructions on how to install locally and modified the API_KEY variable for ease of update for other developers

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,12 +5,13 @@ export const OPEN_MODAL = 'OPEN_Modal';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
 export const TRENDING_GIFS = 'TRENDING_GIFS';
 
+const API_KEY = "AEUt8z3mb7Hz69PBW3IYWJCKG0tTjF9k";
 const SEARCH_API_URL = "https://api.giphy.com/v1/gifs/search?rating=G&lang=en&offset=0&q=";
 const TRENDING_API_URL = "https://api.giphy.com/v1/gifs/trending?limit=25&rating=G"
-const API_KEY = "&api_key=AEUt8z3mb7Hz69PBW3IYWJCKG0tTjF9k";
+const API_KEY_QUERY = `&api_key=${API_KEY}`;
 
 export function requestGifs(term = null) {
-	const data = request.get(`${SEARCH_API_URL}${term.replace(/\s/g, '+')}${API_KEY}`);
+	const data = request.get(`${SEARCH_API_URL}${term.replace(/\s/g, '+')}${API_KEY_QUERY}`);
 
 	return {
 		type: REQUEST_GIFS,
@@ -19,7 +20,7 @@ export function requestGifs(term = null) {
 }
 
 export function trendingGifs(term=null) {
-	const data = request.get(`${TRENDING_API_URL}${API_KEY}`);
+	const data = request.get(`${TRENDING_API_URL}${API_KEY_QUERY}`);
 
 	return {
 		type: TRENDING_GIFS,
